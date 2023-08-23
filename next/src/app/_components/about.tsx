4b84735242ccfbe6a27ad89bac3d9a8f0cf35262
NathanHuey <nathan@pipeiq.ai>
2023-08-23 00:03:59 -0700

    feat: add landing

---
+++ next/src/app/_components/about.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <>
+      <div className='flex h-screen flex-col items-center justify-center'>
+        <h1 className='text-5xl font-bold text-neutral-focus'>
+          A little more about me.
+        </h1>
+      </div>
+    </>
+  );
+}
--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,11 +1,20 @@
+import Image from 'next/image';
 import Navbar from './_components/navbar';
+import Landing from './_components/landing';
+import About from './_components/about';
+import arrow from 'public/arrow.svg';
 
 export default function Home() {
   return (
     <>
-      <div className='flex h-screen'>
+      <div className='flex h-screen flex-col justify-between'>
         <Navbar />
+        <Landing />
+        <div className='mb-2 flex w-screen justify-center'>
+          <Image src={arrow} alt='arrow' height={50} width={50} />
+        </div>
       </div>
+      <About />
     </>
   );
 }
