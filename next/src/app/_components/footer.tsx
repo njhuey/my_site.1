112dd462e376daa9f35080be073125be236a5b22
NathanHuey <nathan@pipeiq.ai>
2023-09-02 20:49:11 -0700

    feat: add footer

---
+++ next/src/app/_components/footer.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import nextjs from 'public/nextjs.svg';
+import tailwindcss from 'public/tailwindcss.svg';
+
+export default function Footer() {
+  return (
+    <div className='mt-32 flex w-full flex-col items-center px-8 py-4'>
+      <div className='flex w-full flex-row justify-evenly'>
+        <Image src={nextjs} alt='nextjs' width={150} height={100} />
+        <Image src={tailwindcss} alt='tailwindcss' width={200} height={100} />
+      </div>
+      <h1 className='text-xl mb-8'>Nathan Huey &#169; 2022-2022</h1>
+    </div>
+  );
+}
