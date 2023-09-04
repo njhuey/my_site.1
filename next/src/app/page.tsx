8a96f8bda47da6bd5331039ebe8dd90e124c2573
NathanHuey <nathan@pipeiq.ai>
2023-09-04 16:36:28 -0700

    feat: animated arrow

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image';
 import Navbar from './_components/navbar';
 import Landing from './_components/landing';
+import Arrow from './_components/arrow';
 import About from './_components/about';
 import Projects from './_components/projects';
 import Footer from './_components/footer';
-import arrow from 'public/arrow.svg';
 
 export default function Home() {
   return (
@@ -12,9 +11,7 @@ export default function Home() {
       <div className='flex h-screen flex-col justify-between'>
         <Navbar />
         <Landing />
-        <div className='mb-2 flex w-screen justify-center'>
-          <Image src={arrow} alt='arrow' height={50} width={50} />
-        </div>
+        <Arrow />
       </div>
       <About />
       <Projects />
