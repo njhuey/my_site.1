0eace5c1e0be9deec6bb883044e3e12dc24fc03f
Nathan Huey <nate.huey4@gmail.com>
2023-09-05 13:25:50 -0700

    feat: about section interactive

--- next/src/app/_components/about.tsx
+++ next/src/app/_components/about.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 export default function About() {
   return (
-    <div className='mt-32 flex flex-row'>
-      <div className='flex w-1/2 flex-col flex-wrap items-center justify-center'>
-        <div className='max-w-2xl p-12'>
-          <h1 className='mb-6 text-5xl font-bold text-neutral-focus'>
+    <div className='mt-32 flex flex-row flex-wrap items-center justify-around'>
+      <div className='min-w-2xl w-2xl flex flex-col items-center justify-center'>
+        <div className='w-2xl max-w-2xl p-6'>
+          <h1 className='text-center text-3xl font-bold text-neutral-focus sm:mb-6 md:text-5xl'>
             A little more about me.
           </h1>
-          <p className='py-2 text-xl'>
+          <p className='py-2 sm:text-xl'>
             I first became fascinated by programming when exposed to the{' '}
             <Link
               href='https://www.geeksforgeeks.org/the-knights-tour-problem/'
@@ -21,14 +21,14 @@ export default function About() {
             algorithm problem in high school. I'm interested in all things
             programming and always looking to learn something new.
           </p>
-          <p className='py-2 text-xl'>
+          <p className='py-2 sm:text-xl'>
             Most recently, I've gotten the opportunity to work on multiple web
             applications, both in industry and in university affiliated
             organizations. I'm currently searching for roles where I can
             strength my abilities as a software engineer and contribute to
             something meaningful.
           </p>
-          <p className='py-2 text-xl'>
+          <p className='py-2 sm:text-xl'>
             When I'm not coding, I enjoy skiing, rock climbing, and sometimes
             golf. Additionally, I've been a snare drummer for 8 years, which
             landed me the opportunity to be a part of the 49er drumline, Niner
@@ -36,13 +36,13 @@ export default function About() {
           </p>
         </div>
       </div>
-      <div className='min-w-xl flex w-1/2 items-center p-12'>
+      <div className='p-4'>
         <Image
-          src='/hackuci.jpg'
+          src='/hackuci.jpeg'
           alt='hackuci'
           width={800}
           height={600}
-          className='h-[433px] w-[700px] rounded-3xl shadow-2xl'
+          className='h-[100] w-[150] rounded-3xl shadow-2xl '
         />
       </div>
     </div>
