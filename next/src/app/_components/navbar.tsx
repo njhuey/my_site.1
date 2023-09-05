e4a852969a53aeeb95e3ed333be4aeae1f756eff
NathanHuey <nathan@pipeiq.ai>
2023-09-05 09:10:47 -0700

    feat: navbar buttons

--- next/src/app/_components/navbar.tsx
+++ next/src/app/_components/navbar.tsx
@@ -1,24 +1,45 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { RefObject } from 'react';
 import downloadIcon from 'public/download.svg';
 
-export default function Navbar() {
+interface NavbarProps {
+  aboutRef: RefObject<HTMLDivElement>;
+  projectsRef: RefObject<HTMLDivElement>;
+}
+
+export default function Navbar({ aboutRef, projectsRef }: NavbarProps) {
   return (
     <div className='flex w-full justify-end px-8 py-4'>
       <div className='hidden sm:block'>
-        <button className='btn btn-ghost m-2 text-lg font-medium normal-case'>
+        <button
+          className='btn btn-ghost m-2 text-lg font-medium normal-case'
+          onClick={() =>
+            aboutRef.current?.scrollIntoView({ behavior: 'smooth' })
+          }
+        >
           About
         </button>
-        <button className='btn btn-ghost m-2 text-lg font-medium normal-case'>
+        <button
+          className='btn btn-ghost m-2 text-lg font-medium normal-case'
+          onClick={() =>
+            projectsRef.current?.scrollIntoView({ behavior: 'smooth' })
+          }
+        >
           Projects
         </button>
-        <button className='btn btn-secondary m-2 text-lg font-medium normal-case'>
-          R&#233;sum&#233;
-        </button>
+        <Link href='/Nathan Huey Resume 2023.pdf' target='_blank'>
+          <button className='btn btn-secondary m-2 text-lg font-medium normal-case'>
+            R&#233;sum&#233;
+          </button>
+        </Link>
       </div>
       <div className='md:hidden'>
-        <button className='btn btn-ghost m-1 text-lg font-medium normal-case'>
-          <Image src={downloadIcon} alt='Download' />
-        </button>
+        <Link href='/Nathan Huey Resume 2023.pdf' target='_blank'>
+          <button className='btn btn-ghost m-1 text-lg font-medium normal-case'>
+            <Image src={downloadIcon} alt='Download' />
+          </button>
+        </Link>
       </div>
     </div>
   );
--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useRef } from 'react';
 import Navbar from './_components/navbar';
 import Landing from './_components/landing';
 import Arrow from './_components/arrow';
@@ -6,15 +8,22 @@ import Projects from './_components/projects';
 import Footer from './_components/footer';
 
 export default function Home() {
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
+
   return (
     <>
       <div className='flex h-screen flex-col justify-between'>
-        <Navbar />
+        <Navbar aboutRef={aboutRef} projectsRef={projectsRef} />
         <Landing />
         <Arrow />
       </div>
-      <About />
-      <Projects />
+      <div ref={aboutRef}>
+        <About />
+      </div>
+      <div ref={projectsRef}>
+        <Projects />
+      </div>
       <Footer />
     </>
   );
