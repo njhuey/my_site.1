c1acbec63385342e9aa94d8e127125ed6631ef4a
NathanHuey <nathan@pipeiq.ai>
2023-09-05 16:01:16 -0700

    feat: interactive footer

--- next/src/app/_components/footer.tsx
+++ next/src/app/_components/footer.tsx
@@ -4,12 +4,20 @@ import tailwindcss from 'public/tailwindcss.svg';
 
 export default function Footer() {
   return (
-    <div className='mt-32 flex w-full flex-col items-center px-8 py-4'>
-      <div className='flex w-full flex-row justify-evenly'>
-        <Image src={nextjs} alt='nextjs' width={150} height={100} />
-        <Image src={tailwindcss} alt='tailwindcss' width={200} height={100} />
+    <div className='mt-24 flex w-full flex-col items-center px-8 py-4'>
+      <div className='hidden w-full sm:block'>
+        <div className='flex w-full flex-row justify-evenly '>
+          <Image src={nextjs} alt='nextjs' width={150} height={100} />
+          <Image src={tailwindcss} alt='tailwindcss' width={200} height={100} />
+        </div>
       </div>
-      <h1 className='mb-8 text-xl'>Nathan Huey &#169; 2022-2023</h1>
+      <div className='w-full sm:hidden'>
+        <div className='flex w-full flex-row justify-around'>
+          <Image src={nextjs} alt='nextjs' width={100} height={50} />
+          <Image src={tailwindcss} alt='tailwindcss' width={150} height={50} />
+        </div>
+      </div>
+      <h1 className='mb-8 sm:text-xl'>Nathan Huey &#169; 2022-2023</h1>
     </div>
   );
 }
