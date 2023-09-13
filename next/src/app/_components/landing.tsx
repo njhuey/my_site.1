9475524c18edc317de82b0b66fe5fd27963d8e4f
Nathan Huey <nate.huey4@gmail.com>
2023-09-13 14:03:22 -0700

    feat: github and linkedin links interactive

--- next/src/app/_components/landing.tsx
+++ next/src/app/_components/landing.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 import linkedin from 'public/linkedin.svg';
 import github from 'public/github.svg';
 
@@ -21,24 +24,28 @@ export default function Landing() {
         California, Irvine
       </p>
       <div className='mt-2 flex flex-row'>
-        <Link href='https://www.linkedin.com/in/nhuey' target='_blank'>
-          <Image
-            src={linkedin}
-            alt='linkedin'
-            width={40}
-            height={40}
-            className='m-1'
-          />
-        </Link>
-        <Link href='https://github.com/njhuey' target='_blank'>
-          <Image
-            src={github}
-            alt='github'
-            width={30}
-            height={30}
-            className='mx-1 mt-2'
-          />
-        </Link>
+        <motion.div whileHover={{ scale: 1.2 }}>
+          <Link href='https://www.linkedin.com/in/nhuey' target='_blank'>
+            <Image
+              src={linkedin}
+              alt='linkedin'
+              width={40}
+              height={40}
+              className='m-1'
+            />
+          </Link>
+        </motion.div>
+        <motion.div whileHover={{ scale: 1.2 }}>
+          <Link href='https://github.com/njhuey' target='_blank'>
+            <Image
+              src={github}
+              alt='github'
+              width={30}
+              height={30}
+              className='mx-1 mt-2'
+            />
+          </Link>
+        </motion.div>
       </div>
     </div>
   );
